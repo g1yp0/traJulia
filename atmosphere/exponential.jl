aba892ded58670b8965232e62d469358fe1159e4
glypo <151966490+g1yp0@users.noreply.github.com>
2024-10-29 20:33:59 +0000

    Adding Unitful to exponential.jl
    Making the whole project use Unitful

--- atmosphere/exponential.jl
+++ atmosphere/exponential.jl
@@ -1,22 +1,26 @@
 module AtmosphereExponential
 
+using Unitful
+using Unitful.DefaultSymbols  # For unit symbols like m, kg, etc.
+
 """
 This module provides a low-fidelity model of Earth's atmopsheric density. 
 
 It utilises a simple exponential atmosphere model from:
-- Regan, F. J. (1993). *Dynamics of Atmospheric Re-Entry*. AIAA Education Series.
+- Regan, F. J. (1993). 'Dynamics of Atmospheric Re-Entry'.
 
 # Arguments
-- `altitude`: Altitude above sea level in metres (m).
+- `altitude`: Altitude above sea level in meters (m), with units (e.g., `1000u"m"`).
 
 Note, this altitude is geometric, not geopotential.
 
 # Returns
-- Atmospheric density in kilograms per cubic metre (kg/m³).
+- Atmospheric density (ρ) in kilograms per cubic metre (kg/m³).
 """
-function atmosphere(altitude)
-    ρ0 = 1.225          # Sea level standard density (kg/m³)
-    h_scale = 8434      # Scale height (m) from Regan
+
+function atmosphere(altitude::Quantity{<:Real, Unitful.𝐋})
+    ρ0 = 1.225u"kg/m^3"          # Sea level standard density (kg/m³)
+    h_scale = 6700u"m"           # Scale height from Regan
     ρ = ρ0 * exp(-altitude / h_scale)
     return ρ
 end
